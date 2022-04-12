a3fe282fe2bd26f3a4705d42571ea9886aeb2ac1
JMinkyoung <whk5817@gmail.com>
2022-04-12 19:39:03 +0900

    Background 구현 시작

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import HeaderWrapper from './components/Header/HeaderWrapper.js'
 import ContentWrapper from './components/Content/ContentWrapper.js';
+import Background from './components/Background.js';
 
 export default function App($app){
   this.state = {};
 
-
+  const background = new Background($app);
   const profileHeader = new HeaderWrapper({$app, initialState: this.state});
   const profileContent = new ContentWrapper({$app, initialState: this.state});
 
