d261debc088a0e3190dc30c142b651ecd11a97e2
JMinkyoung <whk5817@gmail.com>
2022-04-14 19:25:46 +0900

    Portfolio Main, Steam 버전 분리

--- src/main/components/Background.js
+++ src/main/components/Background.js
@@ -1,11 +1,11 @@
 export default function Background($app) {
   this.$target = document.createElement('div');
   this.$target.className="BackgroundWrapper";
-  this.$target.style.cssText = 'width: 100%; height:100%; position:fixed; z-index: 0; item-align: center';
+  // this.$target.style.cssText = 'width: 100%; height:100%;';
 
   this.$text = document.createElement('span');
   this.$text.className="BackgroundText underline";
-  this.$text.style.cssText = 'font-size: 100px; font-weight:bolder';
+  this.$text.style.cssText = 'font-size: 100px; font-weight:bolder;';
   this.$text.innerHTML="Frontend Engineer<br/>좌민경 입니다.";
 
   this.$target.appendChild(this.$text);
